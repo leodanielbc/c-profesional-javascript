f2f102a0ecf028a0270f34b3447ecd593a87a674
leodanielbc <benitez.leonardp06@gmail.com>
2020-01-10 00:32:59 -0500

    tipos basicos en typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -5,3 +5,32 @@ function add(a: number, b:number) {
 }
 const sum = add(2, 3);
 console.log(sum);
+
+let muted: boolean = true;
+
+// arreglos
+
+let people: string[] = [];
+people = ["Isabel", "Nicole", "Maria"];
+
+people.push("Leo");
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push("Leo");
+peopleAndNumbers.push(89);
+
+// enum
+enum Color {
+    Rojo = "Rojo",
+    Verde = "Verde",
+    Azul = "Azul"
+}
+let colorFavorite: Color = Color.Azul;
+console.log("My color favorite is: "+colorFavorite);
+
+// any
+let commodin: any = "jocker";
+commodin = { type: 5 };
+
+// object
+let someObject: object = { age: 5 };
